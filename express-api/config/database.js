273bbbdc533391341cb4a6fedeedc20a515c2f25
errics7 <ebernedy@gmail.com>
2026-07-20 11:56:59 +0700

    refactor config database

--- express-api/config/database.js
+++ express-api/config/database.js
@@ -1,21 +1,14 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "etaxzone_db",
-  charset: "utf8",
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    return;
-  }
-  console.log("Database connected successfully");
-  connection.release();
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: "utf8",
 });
 
 module.exports = pool;
--- express-api/config/sequelizeconf.js
+++ express-api/config/sequelizeconf.js
@@ -1,24 +1,23 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
 
 const sequelizeConf = new Sequelize(
-  "etaxzone_db", // Nama database yang diubah
-  "root",       // Username MySQL
-  "",          // Password dikosongkan (default di Windows, ubah jika perlu)
-  {
-    host: "127.0.0.1",  // Host server
-    dialect: "mysql",   // Dialek database
-    port: 3306,         // Port default MySQL di Windows
-    define: {
-      charset: "utf8", // Charset
-    },
-    timezone: "+07:00", // Timezone database (WIB)
-    retry: { max: 30 }, // Retry konfigurasi
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        timezone: "+07:00",
+        define: {
+            charset: "utf8",
+        },
+        retry: {
+            max: 30,
+        },
+    }
 );
-
-sequelizeConf
-  .authenticate()
-  .then(() => console.log("Database connected successfully test"))
-  .catch((err) => console.error("Database connection failed:", err));
 
 module.exports = sequelizeConf;
